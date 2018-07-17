ff56911ca943145efe715269213e126083964651
takeshi-kimura <kimura681@gmail.com>
2018-07-17 14:12:17 +0900

    add allowed_host

--- first/first/settings.py
+++ first/first/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pesk((!%se^x8y+ey1n74-)b-7qsge@wgn5yozw)zqt!4yszi+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['153.126.197.114']
 
 
 # Application definition
